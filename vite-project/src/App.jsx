68cd0fbe6e65de45113fa491335cb7fea9adada5
DESKTOP-6P7GRMM\fukud <fukudaum@gmail.com>
2023-03-11 12:45:27 -0300

    Starting studying react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        author="Gustavo Fukuda"
+        content="Lorem Ipsum lalalalllal alla ala al alalalalalla"
+      />
+    </div>
+  )
+}
