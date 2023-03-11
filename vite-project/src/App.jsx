2a909a4d6628555d2825492edaf33f39ab0808b5
DESKTOP-6P7GRMM\fukud <fukudaum@gmail.com>
2023-03-11 13:55:48 -0300

    Creating sidebar component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,23 @@
-import { Header } from "./components/Header"
-import { Post } from "./Post"
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
 
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
-  return (
-    <div>
-      <Header/>
-      <Post 
-        author="Gustavo Fukuda"
-        content="Lorem Ipsum lalalalllal alla ala al alalalalalla"
-      />
-    </div>
-  )
+    return (
+        <div>
+            <Header/>
+            <div className={styles.wrapper}>
+                <Sidebar/>
+                <main>
+                    <Post 
+                        author="Gustavo Fukuda"
+                        content="Lorem Ipsum lalalalllal alla ala al alalalalalla"
+                    />
+                </main>
+            </div>
+        </div>
+    )
 }
